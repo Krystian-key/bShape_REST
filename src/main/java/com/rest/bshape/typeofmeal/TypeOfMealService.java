1f99d8c1882f567b98becdca995e45f24bdc503e
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-10-06 21:10:05 +0200

    Revert "Added additional repository for service connection"
    This reverts commit 44a2f20b

--- src/main/java/com/rest/bshape/typeofmeal/TypeOfMealService.java
+++ src/main/java/com/rest/bshape/typeofmeal/TypeOfMealService.java
@@ -1,20 +1,76 @@
 package com.rest.bshape.typeofmeal;
 
-import com.rest.bshape.typeofmeal.domain.TypeOfMeal;
-import com.rest.bshape.typeofmeal.domain.TypeOfMealID;
+import com.rest.bshape.exception.ResourceNotFoundException;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface TypeOfMealService {
+@Service
+public class TypeOfMealService {
 
-    List<TypeOfMeal> findAll();
+    private final TypeOfMealRepository typeOfMealRepository;
 
-    TypeOfMeal findById(Long id);
+    public TypeOfMealService(TypeOfMealRepository typeOfMealRepository) {
+        this.typeOfMealRepository = typeOfMealRepository;
+    }
 
-    TypeOfMealID create(TypeOfMeal typeOfMeal);
 
-    TypeOfMeal update(TypeOfMeal typeOfMeal, Long id);
+    public List<TypeOfMealDTO> findAll() {
+        List<TypeOfMeal> optionalTypeOfMeal = this.typeOfMealRepository.findAll();
+        return optionalTypeOfMeal.isEmpty() ? Collections.emptyList() : optionalTypeOfMeal.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 
-    void delete(Long id);
 
+    public Optional<TypeOfMealDTO> findById(Long id) {
+        Optional<TypeOfMeal> optionalTypeOfMeal = typeOfMealRepository.findById(id);
+        return optionalTypeOfMeal.isEmpty() ? Optional.empty() : optionalTypeOfMeal.map(this::convertToDTO);
+    }
+
+    public Optional<TypeOfMealID> create(TypeOfMealDTO typeOfMealDTO) {
+        TypeOfMeal typeOfMeal = this.convertFromDTO(typeOfMealDTO);
+
+        TypeOfMeal createdTypeOfMeal = typeOfMealRepository.save(typeOfMeal);
+        val typeOfMealID = new TypeOfMealID(createdTypeOfMeal.getId());
+        return Optional.of(typeOfMealID);
+    }
+
+    public Optional<TypeOfMealDTO> update(TypeOfMealDTO typeOfMealDTO, Long id) {
+        TypeOfMeal typeOfMeal = this.convertFromDTO(typeOfMealDTO);
+
+        Optional<TypeOfMeal> typeOfMealById = typeOfMealRepository.findById(id);
+        if (typeOfMealById.isEmpty()) {
+            return Optional.empty();
+        }
+        TypeOfMeal existingTypeOfMeal = typeOfMealById.get();
+        existingTypeOfMeal.setTypeMeals(typeOfMeal.getTypeMeals());
+        return Optional.of(this.convertToDTO(typeOfMealRepository.save(existingTypeOfMeal)));
+    }
+
+    public ResponseEntity<TypeOfMealID> delete(Long id) {
+        TypeOfMeal existingMeal = this.typeOfMealRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Type of meal was not found, with id: " + id));
+        this.typeOfMealRepository.delete(existingMeal);
+        return ResponseEntity.ok().build();
+    }
+
+    private TypeOfMealDTO convertToDTO(TypeOfMeal typeOfMeal) {
+        return TypeOfMealDTO.builder()
+                .id(typeOfMeal.getId())
+                .typeMeals(typeOfMeal.getTypeMeals())
+                .build();
+    }
+
+    private TypeOfMeal convertFromDTO(TypeOfMealDTO typeOfMealDTO) {
+        return TypeOfMeal.builder()
+                .id(typeOfMealDTO.getId())
+                .typeMeals(typeOfMealDTO.getTypeMeals())
+                .build();
+    }
 }
