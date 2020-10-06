e9388dd3fcecd827ad4d18fc0dd242a6ba48fef8
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-10-06 21:33:18 +0200

    Resolved Problem

--- src/main/java/com/rest/bshape/userTarget/UserTargetRepository.java
+++ src/main/java/com/rest/bshape/userTarget/UserTargetRepository.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @CrossOrigin(origins = "http://localhost:4200")
-public interface TargetRepository extends JpaRepository<Target, Long> {
+public interface UserTargetRepository extends JpaRepository<UserTarget, Long> {
 }
