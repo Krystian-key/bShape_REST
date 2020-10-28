1bc4efc79e9342bee4cc2db33addb100519e62d1
Jerzy Pałka <57533018+kudini@users.noreply.github.com>
2020-10-28 16:52:08 +0100

    initial commit

---
+++ src/test/java/com/rest/bshape/product/ProductControllerTest.java
@@ -0,0 +1,28 @@
+package com.rest.bshape.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
